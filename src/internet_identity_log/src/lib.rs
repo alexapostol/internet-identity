e73c8e25ff4639e1564554d3f7029fec3d108b5b
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2022-09-08 14:50:28 +0200

    WIP

--- src/internet_identity_log/src/lib.rs
+++ src/internet_identity_log/src/lib.rs
@@ -1,10 +1,40 @@
 use candid::Principal;
 use ic_cdk::api;
 use ic_cdk_macros::query;
+use stable_structures::memory_manager::MemoryId;
+use stable_structures::{DefaultMemoryImpl, RestrictedMemory, WASM_PAGE_SIZE};
 
-// TODO: implement canister.
+type Memory = RestrictedMemory<DefaultMemoryImpl>;
 
-#[query]
-fn whoami() -> Principal {
-    api::caller()
+const GIB: u64 = 1 << 30;
+/// The maximum number of Wasm pages that we allow to use for the stable storage.
+const NUM_WASM_PAGES: u64 = 4 * GIB / WASM_PAGE_SIZE;
+
+const LOG_INDEX_MEMORY_ID: MemoryId = MemoryId::new(0);
+const LOG_DATA_MEMORY_ID: MemoryId = MemoryId::new(1);
+
+/// Creates a memory region for the configuration stable cell.
+fn config_memory() -> Memory {
+    RestrictedMemory::new(DefaultMemoryImpl::default(), 0..1)
+}
+
+/// Creates a memory region for the append-only block list.
+fn blocks_memory() -> Memory {
+    RestrictedMemory::new(DefaultMemoryImpl::default(), 1..NUM_WASM_PAGES)
+}
+
+thread_local! {
+    /// Static configuration of the archive that init() sets once.
+    static CONFIG: RefCell<ConfigCell> = RefCell::new(ConfigCell::init(
+        config_memory(),
+        ArchiveConfig::default(),
+    ).expect("failed to initialize stable cell"));
+
+    /// Static memory manager to manage the memory available for blocks.
+    static MEMORY_MANAGER: RefCell<MemoryManager<Memory>> = RefCell::new(MemoryManager::init(blocks_memory()));
+
+    /// Append-only list of encoded blocks stored in stable memory.
+    static BLOCKS: RefCell<BlockLog> = with_memory_manager(|memory_manager| {
+        RefCell::new(Log::init(memory_manager.get(LOG_INDEX_MEMORY_ID), memory_manager.get(LOG_DATA_MEMORY_ID)).expect("failed to initialize stable log"))
+    });
 }
