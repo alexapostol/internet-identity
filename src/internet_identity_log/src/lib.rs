6cda839acec1a02ff7cfcb9035f345f7c4c2c965
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2022-09-08 15:35:14 +0200

    WIP 2

--- src/internet_identity_log/src/lib.rs
+++ src/internet_identity_log/src/lib.rs
@@ -1,10 +1,18 @@
 use candid::Principal;
-use ic_cdk::api;
+use ic_cdk::{api, caller, trap};
 use ic_cdk_macros::query;
-use stable_structures::memory_manager::MemoryId;
-use stable_structures::{DefaultMemoryImpl, RestrictedMemory, WASM_PAGE_SIZE};
+use internet_identity_interface::*;
+use serde_bytes::ByteBuf;
+use stable_structures::memory_manager::{ManagedMemory, MemoryId, MemoryManager};
+use stable_structures::{
+    cell::Cell as StableCell, log::Log, DefaultMemoryImpl, RestrictedMemory, WASM_PAGE_SIZE,
+};
+use std::borrow::Borrow;
+use std::cell::RefCell;
 
 type Memory = RestrictedMemory<DefaultMemoryImpl>;
+type StableLog = Log<ManagedMemory<Memory>, ManagedMemory<Memory>>;
+type ConfigCell = StableCell<ArchiveConfig, Memory>;
 
 const GIB: u64 = 1 << 30;
 /// The maximum number of Wasm pages that we allow to use for the stable storage.
@@ -12,6 +20,28 @@ const NUM_WASM_PAGES: u64 = 4 * GIB / WASM_PAGE_SIZE;
 
 const LOG_INDEX_MEMORY_ID: MemoryId = MemoryId::new(0);
 const LOG_DATA_MEMORY_ID: MemoryId = MemoryId::new(1);
+const USER_INDEX_MEMORY_ID: MemoryId = MemoryId::new(2);
+
+thread_local! {
+    /// Static configuration of the archive that init() sets once.
+    static CONFIG: RefCell<ConfigCell> = RefCell::new(ConfigCell::init(
+        config_memory(),
+        LogConfig::default(),
+    ).expect("failed to initialize stable cell"));
+
+    /// Static memory manager to manage the memory available for blocks.
+    static MEMORY_MANAGER: RefCell<MemoryManager<Memory>> = RefCell::new(MemoryManager::init(blocks_memory()));
+
+    /// Append-only list of encoded blocks stored in stable memory.
+    static LOG: RefCell<StableLog> = with_memory_manager(|memory_manager| {
+        RefCell::new(Log::init(memory_manager.get(LOG_INDEX_MEMORY_ID), memory_manager.get(LOG_DATA_MEMORY_ID)).expect("failed to initialize stable log"))
+    });
+
+    ///
+    static USER_INDEX: RefCell<StableLog> = with_memory_manager(|memory_manager| {
+        RefCell::new(Log::init(memory_manager.get(LOG_INDEX_MEMORY_ID), memory_manager.get(LOG_DATA_MEMORY_ID)).expect("failed to initialize stable log"))
+    });
+}
 
 /// Creates a memory region for the configuration stable cell.
 fn config_memory() -> Memory {
@@ -23,18 +53,39 @@ fn blocks_memory() -> Memory {
     RestrictedMemory::new(DefaultMemoryImpl::default(), 1..NUM_WASM_PAGES)
 }
 
-thread_local! {
-    /// Static configuration of the archive that init() sets once.
-    static CONFIG: RefCell<ConfigCell> = RefCell::new(ConfigCell::init(
-        config_memory(),
-        ArchiveConfig::default(),
-    ).expect("failed to initialize stable cell"));
+/// A helper function to access the configuration.
+fn with_config<R>(f: impl FnOnce(&LogConfig) -> R) -> R {
+    CONFIG.with(|cell| f(cell.borrow().get()))
+}
 
-    /// Static memory manager to manage the memory available for blocks.
-    static MEMORY_MANAGER: RefCell<MemoryManager<Memory>> = RefCell::new(MemoryManager::init(blocks_memory()));
+/// A helper function to access the memory manager.
+fn with_memory_manager<R>(f: impl FnOnce(&MemoryManager<Memory>) -> R) -> R {
+    MEMORY_MANAGER.with(|cell| f(&*cell.borrow()))
+}
 
-    /// Append-only list of encoded blocks stored in stable memory.
-    static BLOCKS: RefCell<BlockLog> = with_memory_manager(|memory_manager| {
-        RefCell::new(Log::init(memory_manager.get(LOG_INDEX_MEMORY_ID), memory_manager.get(LOG_DATA_MEMORY_ID)).expect("failed to initialize stable log"))
+/// A helper function to access the log.
+fn with_log<R>(f: impl FnOnce(&StableLog) -> R) -> R {
+    LOG.with(|cell| f(&*cell.borrow()))
+}
+
+/// Configuration of the II log canister.
+#[derive(Serialize, Deserialize)]
+struct LogConfig {
+    /// This canister will accept entries only from this principal.
+    ii_canister: Principal,
+}
+
+#[update]
+fn write_entry(user_number: UserNumber, timestamp: Timestamp, entry: ByteBuf) {
+    with_config(|config| {
+        if config.ii_canister != caller() {
+            trap(&format!(
+                "Only {} is allowed to write entries.",
+                config.ii_canister
+            ))
+        }
     });
+    with_log(|log| {
+        log.append(entry.as_ref()).expect("failed"); // TODO: handle failure correctly
+    })
 }
